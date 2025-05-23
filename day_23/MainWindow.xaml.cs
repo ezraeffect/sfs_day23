e151978de17883c351879d1635c52e9ede519907
ezraeffect <build@fx0.kr>
2025-05-23 13:37:21 +0900

    실습. DataGrid

--- day_23/MainWindow.xaml.cs
+++ day_23/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +18,70 @@ namespace day_23
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<Student> students = new List<Student>();
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void openFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            var filePath = string.Empty;
+
+            OpenFileDialog dialog = new()
+            {
+                InitialDirectory = "c:\\",
+                Filter = "CSV File (*.csv)|*.csv",
+                FilterIndex = 1,
+                RestoreDirectory = true
+            };
+
+            // Show open file dialog box
+            bool? result = dialog.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                // Open document
+                filePath = dialog.FileName;
+
+                if (File.Exists(filePath))
+                {
+                    filePathLabel.Content = filePath;
+                }
+            }
+        }
+
+        private void getDataButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (filePathLabel.Content is string filePath && File.Exists(filePath))
+            {
+                using(StreamReader sr = new StreamReader(filePath))
+                {
+                    string headerLine = sr.ReadLine();
+                    for(int i = 0; !sr.EndOfStream; i++)
+                    {
+                        if (sr.ReadLine() is string line)
+                        {
+                            string[] data = line.Split(',');
+                            students.Add(new Student() { Name = data[0], Age = int.Parse(data[1]), Description = data[2] });
+                        }
+                    }
+                }
+                csvDataGrid.ItemsSource = students;
+            }
+            else
+            {
+                MessageBox.Show("경로가 잘못되었거나 파일이 존재하지 않습니다", "오류", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            
+        }
+    }
+
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Description { get; set; }
     }
 }
